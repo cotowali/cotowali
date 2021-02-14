613cad3d652f56b2b8d27c649b067904f196f9f5
zakuro <z@kuro.red>
2021-02-14 17:05:50 +0900

    Pos.merge

--- vash/token/pos.v
+++ vash/token/pos.v
@@ -14,7 +14,26 @@ pub fn new_pos(pos Pos) Pos {
 		line: pos.line
 		col: pos.col
 		offset: pos.offset
-		len: if pos.len == 0 { 1 } else { pos.len }
-		last_line: if pos.last_line == 0 { pos.line } else { pos.last_line }
+		len: if pos.len == 0 {
+			1
+		} else {
+			pos.len
+		}
+		last_line: if pos.last_line == 0 {
+			pos.line
+		} else {
+			pos.last_line
+		}
+	}
+}
+
+pub fn (p1 Pos) merge(p2 Pos) Pos {
+	if p1.offset > p2.offset {
+		return p2.merge(p1)
+	}
+	return Pos{
+		...p1
+		len: p2.offset - p1.offset + p2.len
+		last_line: p2.last_line
 	}
 }
--- vash/token/pos_test.v
+++ vash/token/pos_test.v
@@ -16,3 +16,11 @@ fn test_new_pos() {
 	// auto set len to 1
 	assert new_pos(Pos{}).len == 1
 }
+
+fn test_pos_extend() {
+	p1 := new_pos(line: 1, col: 5, offset: 4, len: 2)
+	p2 := new_pos(line: 5, col: 0, offset: 20, len: 3)
+	result := new_pos(line: 1, col: 5, offset: 4, len: 19, last_line: 5)
+	assert p1.merge(p2) == result
+	assert p2.merge(p1) == result
+}
