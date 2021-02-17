ad8b91b629ee66e945cd6737c36871d5dd3a56a0
zakuro <z@kuro.red>
2021-02-18 04:34:57 +0900

    add gen

---
+++ vash/gen/gen.v
@@ -0,0 +1,28 @@
+module gen
+
+import io
+import vash.ast
+
+pub struct Gen {
+pub:
+	out io.Writer
+}
+
+[inline]
+pub fn new(out io.Writer) Gen {
+	return Gen{
+		out: out
+	}
+}
+
+pub fn (mut g Gen) write(s string) ? {
+	g.out.write(s.bytes()) ?
+}
+
+pub fn (mut g Gen) writeln(s string) ? {
+	g.write(s + '\n') ?
+}
+
+pub fn (mut g Gen) gen(f &ast.File) ? {
+	g.writeln('# file: $f.path') ?
+}
