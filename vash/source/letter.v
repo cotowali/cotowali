583ead1f28e2814bc7bf25bf69ab5e1a1818a96a
zakuro <z@kuro.red>
2021-02-20 14:37:10 +0900

    make letter public

--- vash/source/letter.v
+++ vash/source/letter.v
@@ -1,7 +1,7 @@
 module source
 
-type Letter = string
+pub type Letter = string
 
-fn (c Letter) is_whitespace() bool {
+pub fn (c Letter) is_whitespace() bool {
 	return (c.len == 1 && c[0].is_space() && c[0] !in [`\n`, `\r`]) || c == '　'
 }
