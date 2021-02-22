d9d7dc35da8df14c2e2cc4841b68a04a4cd448a6
zakuro <z@kuro.red>
2021-02-22 10:40:28 +0900

    improve pos

--- vash/pos/pos.v
+++ vash/pos/pos.v
@@ -1,12 +1,26 @@
 module pos
 
+import utils.math { max }
+
 pub struct Pos {
 pub:
 	offset    int
 	len       int = 1
 	line      int = 1
-	col       int = 1
 	last_line int = 1
+	col       int = 1
+	last_col  int = 1
+}
+
+[inline]
+pub fn new(pos Pos) Pos {
+	last_line := max(pos.line, pos.last_line)
+	last_col := if pos.line == last_line { pos.col + pos.len } else { pos.last_col }
+	return Pos{
+		...pos,
+		last_line: last_line,
+		last_col: last_col,
+	}
 }
 
 pub fn (p1 Pos) merge(p2 Pos) Pos {
@@ -17,5 +31,6 @@ pub fn (p1 Pos) merge(p2 Pos) Pos {
 		...p1
 		len: p2.offset - p1.offset + p2.len
 		last_line: p2.last_line
+		last_col: p2.last_col
 	}
 }
--- vash/pos/pos_test.v
+++ vash/pos/pos_test.v
@@ -2,25 +2,29 @@ module pos
 
 fn test_new() {
 	pos := Pos{
-		line: 2
-		col: 2
 		offset: 5
 		len: 2
+		line: 2
 		last_line: 2
+		col: 2
+		last_col: 4
 	}
 	assert new(pos) == pos
 
-	// auto set last_line to line
+	// auto set last_line and last_col
 	assert new(line: 2, col: 2, offset: 5, len: 2) == pos
 
 	// auto set len to 1
 	assert new(Pos{}).len == 1
+
+	// if multiline, don,t owerride last_col
+	assert new(offset: 0, len: 3, line: 1, last_line: 2, col: 1, last_col:1).last_col == 1
 }
 
 fn test_pos_extend() {
 	p1 := new(line: 1, col: 5, offset: 4, len: 2)
-	p2 := new(line: 5, col: 0, offset: 20, len: 3)
-	result := new(line: 1, col: 5, offset: 4, len: 19, last_line: 5)
+	p2 := new(line: 5, col: 1, offset: 20, len: 3)
+	result := new(offset: 4, len: 19, line: 1, last_line: 5, col: 5, last_col: 4)
 	assert p1.merge(p2) == result
 	assert p2.merge(p1) == result
 }
