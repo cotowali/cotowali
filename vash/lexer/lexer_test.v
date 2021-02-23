d6307927b5085ecaec7f181a9fe3fb5b841c22fc
zakuro <z@kuro.red>
2021-02-23 13:13:13 +0900

    lexer: imporve test

--- vash/lexer/lexer_test.v
+++ vash/lexer/lexer_test.v
@@ -1,22 +1,44 @@
 module lexer
 
-import vash.source { Source }
+import vash.source
 import vash.token { Token }
 import vash.pos { Pos }
 
-fn test_lexer() {
-	code := '_🐈_'
-	// Pos{i, line, col, len, last_line, last_col}
-	tokens := [
-		Token{.unknown, '_🐈_', Pos{0, 1, 1, 3, 1, 3}}
-		Token{.eof, '', Pos{3, 1, 4, 1, 1, 4}}
-	]
-	lexer := new(path: '', code: code.ustring())
+[flag]
+enum LexerTestOption {
+	@none
+	ignore_pos
+	ignore_text
+}
 
+struct LexerTester {
+	opts LexerTestOption = .@none
+}
+
+fn (t &LexerTester) run(code string, expected_tokens []Token) {
+	lexer := new(path: '', code: code.ustring())
 	mut i := 0
 	for tok in lexer {
-		expected := tokens[i]
-		assert tok == expected
+		expected := expected_tokens[i]
+		if !(t.opts.has(.ignore_pos) || t.opts.has(.ignore_text)) {
+			assert tok == expected
+		} else {
+			if !t.opts.has(.ignore_pos) {
+				assert tok.pos == expected.pos
+			}
+			if !t.opts.has(.ignore_text) {
+				assert tok.text == expected.text
+			}
+		}
 		i++
 	}
+}
+
+fn test_lexer() {
+	LexerTester{}.run(' _🐈_ a ', [
+		/* Pos{i, line, col, len, last_line, last_col} */
+		Token{.unknown, '_🐈_', Pos{1, 1, 2, 3, 1, 4}},
+		Token{.unknown, 'a', Pos{5, 1, 6, 1, 1, 6}},
+		Token{.eof, '', Pos{7, 1, 8, 1, 1, 8}},
+	])
 }
