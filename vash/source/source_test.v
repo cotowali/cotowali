4dcc65a89ab0bc9a6ffb8aba02a69a1fbdd02d39
zakuro <z@kuro.red>
2021-03-01 18:26:51 +0900

    remove osutil

--- vash/source/source_test.v
+++ vash/source/source_test.v
@@ -1,14 +1,14 @@
 // 🐈 <-- to multibyte character test
 module source
 
-import osutil
+import os
 
 fn test_read_file() {
 	got := must_read_file(@FILE)
-
+	code := os.read_file(@FILE) or { panic(err) }
 	expected := Source{
 		path: @FILE
-		code: osutil.must_read_file(@FILE)
+		code: code
 	}
 
 	assert got == expected
