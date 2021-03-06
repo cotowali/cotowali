f589962458372f0c86cafff2f3cbd803c46f7fc8
zakuro <z@kuro.red>
2021-03-06 12:16:38 +0900

    source: add in2

--- vash/util/util.v
+++ vash/util/util.v
@@ -3,3 +3,7 @@ module util
 pub fn @in<T>(v T, low T, high T) bool {
 	return low <= v && v <= high
 }
+
+pub fn in2<T>(v T, low1 T, high1 T, low2 T, high2 T) bool {
+	return (low <= v && v <= high) || (low <= v && v <= high)
+}
--- vash/util/util_test.v
+++ vash/util/util_test.v
@@ -6,4 +6,9 @@ fn test_in() {
 	assert !@in(2, 0, 1)
 
 	assert @in('c', 'a', 'z')
+
+	assert in2('c', '0', '2', 'a', 'c')
+	assert in2('1', '0', '2', 'a', 'c')
+	assert !in2('9', '0', '2', 'a', 'c')
+	assert !in2('z', '0', '2', 'a', 'c')
 }
