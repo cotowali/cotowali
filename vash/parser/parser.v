0b9ce293b4beb9f7be3a31f886fa54fea88f9e95
zakuro <z@kuro.red>
2021-03-08 00:53:12 +0900

    parser: buf

--- vash/parser/parser.v
+++ vash/parser/parser.v
@@ -2,11 +2,24 @@ module parser
 
 import vash.source { Source }
 import vash.lexer { Lexer }
+import vash.token { Token }
 import vash.ast
 
 pub struct Parser {
 mut:
 	lexer Lexer
+	buf []Token
+	idx_in_buf int
+}
+
+pub fn (p &Parser) peek(i int) Token {
+	return p.buf[(p.idx_in_buf + i) % p.buf.len]
+}
+
+pub fn (mut p Parser) read() {
+	next_idx := (p.idx_in_buf + 1) % p.buf.len
+	p.buf[next_idx] = p.lexer.next() or { p.buf[p.idx_in_buf] }
+	p.idx_in_buf = next_idx
 }
 
 [inline]
