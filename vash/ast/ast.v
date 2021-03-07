9d2edf50eb4837e32854f1132013d9b8e1ccebd6
zakuro <z@kuro.red>
2021-03-08 00:40:10 +0900

    ast

--- vash/ast/ast.v
+++ vash/ast/ast.v
@@ -1,6 +1,43 @@
 module ast
 
+import vash.pos { Pos }
+
 pub struct File {
 pub:
 	path string
+}
+
+type Stmt = FnDecl | Pipeline
+
+pub struct FnDecl {
+	pos Pos
+	name string
+}
+
+pub struct Pipeline {
+	pos Pos
+	commands []Command
+}
+
+pub struct Command {
+	pos Pos
+	expr Expr
+	redirects []Redirect
+}
+
+pub struct Redirect {
+	pos Pos
+}
+
+type Expr = CallExpr | IntLiteral
+
+pub struct CallExpr {
+	pos Pos
+	name string
+	args []Expr
+}
+
+pub struct IntLiteral {
+	pos Pos
+	value int
 }
