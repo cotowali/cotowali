370fcc5c424b86c216a68cf94c63e847ac0dd274
zakuro <z@kuro.red>
2021-03-08 00:43:58 +0900

    ast

--- vash/ast/ast.v
+++ vash/ast/ast.v
@@ -5,39 +5,46 @@ import vash.pos { Pos }
 pub struct File {
 pub:
 	path string
+	stmts []Stmt
 }
 
 type Stmt = FnDecl | Pipeline
 
 pub struct FnDecl {
-	pos Pos
+pub:
+	pos  Pos
 	name string
 }
 
 pub struct Pipeline {
-	pos Pos
+pub:
+	pos      Pos
 	commands []Command
 }
 
 pub struct Command {
-	pos Pos
-	expr Expr
+pub:
+	pos       Pos
+	expr      Expr
 	redirects []Redirect
 }
 
 pub struct Redirect {
+pub:
 	pos Pos
 }
 
 type Expr = CallExpr | IntLiteral
 
 pub struct CallExpr {
-	pos Pos
+pub:
+	pos  Pos
 	name string
 	args []Expr
 }
 
 pub struct IntLiteral {
-	pos Pos
+pub:
+	pos   Pos
 	value int
 }
