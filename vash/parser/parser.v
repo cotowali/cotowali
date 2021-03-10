82e4226c8ba601e17d2a8a41b5a86ec4e64bc02a
zakuro <z@kuro.red>
2021-03-10 16:26:12 +0900

    parser: fix bug of read

--- vash/parser/parser.v
+++ vash/parser/parser.v
@@ -16,10 +16,10 @@ pub fn (p &Parser) peek(i int) Token {
 	return p.buf[(p.idx_in_buf + i) % p.buf.len]
 }
 
-pub fn (mut p Parser) read() {
-	next_idx := (p.idx_in_buf + 1) % p.buf.len
-	p.buf[next_idx] = p.lexer.next() or { p.buf[p.idx_in_buf] }
-	p.idx_in_buf = next_idx
+pub fn (mut p Parser) read() Token {
+	p.buf[p.idx_in_buf] = p.lexer.next() or { p.buf[p.idx_in_buf] }
+	p.idx_in_buf++
+	return p.peek(0)
 }
 
 [inline]
