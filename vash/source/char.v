026fbd21df476584e8ad77b763ce4c361719566e
zakuro <z@kuro.red>
2021-03-10 18:17:44 +0900

    source: fmt

--- vash/source/char.v
+++ vash/source/char.v
@@ -22,7 +22,7 @@ pub fn (c Char) @is(class CharClass) bool {
 		.whitespace { (c.len == 1 && c[0].is_space() && c[0] !in [`\n`, `\r`]) || c == '　' }
 		.alphabet { in2(c[0], `a`, `z`, `A`, `Z`) }
 		.digit { @in(c[0], `0`, `9`) }
-		.hex_digit{ c.@is(.digit) || in2(c[0], `a`, `f`, `A`, `F`) }
+		.hex_digit { c.@is(.digit) || in2(c[0], `a`, `f`, `A`, `F`) }
 		.oct_digit { @in(c[0], `0`, `7`) }
 		.binary_digit { @in(c[0], `0`, `1`) }
 	}
