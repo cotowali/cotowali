f212fe18309d2f7f65e89d7eec79fb3cfb0a9c3b
zakuro <z@kuro.red>
2021-03-11 15:24:57 +0900

    fix test

--- vash/parser/parser_test.v
+++ vash/parser/parser_test.v
@@ -2,24 +2,23 @@ module parser
 
 import vash.lexer
 
-fn test_read_token() {
+fn test_consume_token() {
 	mut p := new(lexer.new(path: '', code: '0 1 2 3 4'))
 	assert p.token(0).text == '0'
 	assert p.token(1).text == '1'
 	assert p.token(2).text == '2'
 
-	t := p.read()
-	assert t.text == '1'
+	p.consume()
 	assert p.token(0).text == '1'
 	assert p.token(1).text == '2'
 	assert p.token(2).text == '3'
 
-	p.read()
+	p.consume()
 	assert p.token(0).text == '2'
 	assert p.token(1).text == '3'
 
-	p.read()
-	p.read()
+	p.consume()
+	p.consume()
 	assert p.token(0).text == '4'
 	assert p.token(1).kind == .eof
 	assert p.token(2).kind == .eof
