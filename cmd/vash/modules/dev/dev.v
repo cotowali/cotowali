58707730bbc573cf2f097150ce18886fbd41d77b
zakuro <z@kuro.red>
2021-03-11 15:24:19 +0900

    cmd: print ast

--- cmd/vash/modules/dev/dev.v
+++ cmd/vash/modules/dev/dev.v
@@ -2,8 +2,10 @@ module dev
 
 import cli { Command }
 import strings
+import vash.parser
 import vash.lexer
 import vash.source
+import vash.ast
 
 const (
 	tokens = Command{
@@ -18,11 +20,16 @@ const (
 			return
 		}
 	}
+
 	ast = Command {
 		name: 'ast'
 		description: 'print ast'
 		execute: fn (cmd Command) ? {
-			cmd.execute_help()
+			if cmd.args.len == 0 {
+				cmd.execute_help()
+				return
+			}
+			print_files_ast(cmd.args)
 			return
 		}
 	}
@@ -45,6 +52,22 @@ fn print_files_tokens(paths []string) {
 	sb.writeln(']')
 	println(sb)
 
+}
+
+fn print_files_ast(paths []string) {
+	mut sb := strings.new_builder(100)
+	sb.writeln('[')
+	for path in paths {
+		f := parser.parse_file(path) or {
+			sb.writeln('    ERROR')
+			continue
+		}
+		for line in f.str().split_into_lines() {
+			sb.writeln('    ${line}')
+		}
+	}
+	sb.writeln(']')
+	println(sb)
 }
 
 pub const (
