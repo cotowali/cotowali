a83b20fcbbbf7e011af58e52da0c759568b49dac
zakuro <z@kuro.red>
2021-03-11 14:57:18 +0900

    cmd: print tokens

--- cmd/vash/modules/dev/dev.v
+++ cmd/vash/modules/dev/dev.v
@@ -1,13 +1,20 @@
 module dev
 
 import cli { Command }
+import strings
+import vash.lexer
+import vash.source
 
 const (
 	tokens = Command{
 		name: 'tokens'
 		description: 'print tokens'
 		execute: fn (cmd Command) ? {
-			cmd.execute_help()
+			if cmd.args.len == 0 {
+				cmd.execute_help()
+				return
+			}
+			print_files_tokens(cmd.args)
 			return
 		}
 	}
@@ -20,6 +27,25 @@ const (
 		}
 	}
 )
+
+fn print_files_tokens(paths []string) {
+	mut sb := strings.new_builder(100)
+	sb.writeln('[')
+	for path in paths {
+		sb.writeln(path)
+		s := source.read_file(path) or {
+			sb.writeln('    ERROR')
+			continue
+		}
+		for token in lexer.new(s) {
+			text := token.text.replace_each(['\r', r'\r', '\n', r'\n'])
+			sb.writeln('    .$token.kind $text')
+		}
+	}
+	sb.writeln(']')
+	println(sb)
+
+}
 
 pub const (
 	command = Command{
