46ca1318d4f981bd22d20ec8482458b460420d37
zakuro <z@kuro.red>
2021-03-12 18:06:17 +0900

    cmd: vash run command

--- cmd/vash/main.v
+++ cmd/vash/main.v
@@ -7,8 +7,25 @@ import vash.compiler { new_file_compiler }
 import dev
 
 fn execute_compile(cmd Command) ? {
-	for f in cmd.args {
-		c := new_file_compiler(f) ?
+	if cmd.args.len == 0 {
+		cmd.execute_help()
+		return none
+	}
+
+	is_run := cmd.args[0] == 'run'
+	if is_run {
+		if cmd.args.len != 2 {
+			cmd.execute_help()
+			return none
+		}
+		c := new_file_compiler(cmd.args[1]) ?
+		c.run() ?
+	} else {
+		if cmd.args.len != 1 {
+			cmd.execute_help()
+			return none
+		}
+		c := new_file_compiler(cmd.args[0]) ?
 		c.compile_to_stdout() ?
 	}
 }
@@ -21,7 +38,6 @@ fn main() {
 		version: mod.version
 		sort_flags: false
 		sort_commands: false
-		required_args: 1
 		execute: execute_compile
 		commands: [dev.command]
 	}
