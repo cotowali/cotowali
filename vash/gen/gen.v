05d5cfd92dfd88449df7ba9ae40f24f666f3b5e3
zakuro <z@kuro.red>
2021-03-12 16:57:49 +0900

    gen: first impl

--- vash/gen/gen.v
+++ vash/gen/gen.v
@@ -1,7 +1,7 @@
 module gen
 
 import io
-import vash.ast
+import vash.ast { Stmt, FnDecl, Pipeline }
 
 pub struct Gen {
 pub:
@@ -29,4 +29,48 @@ pub fn (mut g Gen) gen(f ast.File) {
 
 fn (mut g Gen) file(f ast.File) {
 	g.writeln('# file: $f.path')
+	g.stmts(f.stmts)
+}
+
+fn (mut g Gen) stmts(stmts []ast.Stmt){
+	for stmt in stmts {
+		g.stmt(stmt)
+	}
+}
+
+fn (mut g Gen) stmt(stmt ast.Stmt) {
+	match stmt {
+		FnDecl { panic('not implemented') }
+		Pipeline { g.pipeline(stmt) }
+	}
+}
+
+fn (mut g Gen) pipeline(stmt ast.Pipeline) {
+	for i, command in stmt.commands {
+		if i > 0 {
+			g.write(' | ')
+		}
+		g.command(command)
+	}
+	g.writeln('')
+}
+
+fn (mut g Gen) command(cmd ast.Command) {
+	g.expr(cmd.expr)
+}
+
+fn (mut g Gen) expr(expr ast.Expr) {
+	match expr {
+		ast.CallExpr { g.call_expr(expr) }
+		ast.IntLiteral { g.write(expr.token.text) }
+		ast.ErrorNode { panic('error node') }
+	}
+}
+
+fn (mut g Gen) call_expr(expr ast.CallExpr) {
+	g.write(expr.name)
+	for arg in expr.args {
+		g.write(' ')
+		g.expr(arg)
+	}
 }
