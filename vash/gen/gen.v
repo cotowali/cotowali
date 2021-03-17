53aa361f90e95999247d31f0ba3676924a4fdbc7
zakuro <z@kuro.red>
2021-03-18 03:53:58 +0900

    gen: add write_indent

--- vash/gen/gen.v
+++ vash/gen/gen.v
@@ -4,6 +4,9 @@ import io
 import vash.ast { Pipeline, Stmt }
 
 pub struct Gen {
+mut:
+	indent int
+	newline bool = true
 pub:
 	out io.Writer
 }
@@ -15,12 +18,30 @@ pub fn new(out io.Writer) Gen {
 	}
 }
 
+pub fn (mut g Gen) raw_write(bytes []byte) {
+	g.out.write(bytes) or { panic(err) }
+}
+
 pub fn (mut g Gen) write(s string) {
-	g.out.write(s.bytes()) or { panic(err) }
+	$if !prod {
+		if s == '' {
+			panic('writing empty')
+		}
+	}
+	if g.newline {
+		g.write_indent()
+	}
+	bytes := s.bytes()
+	g.raw_write(bytes)
+	g.newline = bytes.last() == `\n`
 }
 
 pub fn (mut g Gen) writeln(s string) {
 	g.write(s + '\n')
+}
+
+pub fn (mut g Gen) write_indent() {
+	g.raw_write('  '.repeat(g.indent).bytes())
 }
 
 pub fn (mut g Gen) gen(f ast.File) {
