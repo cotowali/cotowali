6e654767e1f4d4861b8b2ab3e407d813fd6bc2b6
zakuro <z@kuro.red>
2021-03-19 12:30:05 +0900

    gen: split file

--- vash/gen/gen.v
+++ vash/gen/gen.v
@@ -3,47 +3,6 @@ module gen
 import io
 import vash.ast { Pipeline, Stmt }
 
-pub struct Gen {
-mut:
-	indent  int
-	newline bool = true
-pub:
-	out io.Writer
-}
-
-[inline]
-pub fn new(out io.Writer) Gen {
-	return Gen{
-		out: out
-	}
-}
-
-pub fn (mut g Gen) raw_write(bytes []byte) {
-	g.out.write(bytes) or { panic(err) }
-}
-
-pub fn (mut g Gen) write(s string) {
-	$if !prod {
-		if s == '' {
-			panic('writing empty')
-		}
-	}
-	if g.newline {
-		g.write_indent()
-	}
-	bytes := s.bytes()
-	g.raw_write(bytes)
-	g.newline = bytes.last() == `\n`
-}
-
-pub fn (mut g Gen) writeln(s string) {
-	g.write(s + '\n')
-}
-
-pub fn (mut g Gen) write_indent() {
-	g.raw_write('  '.repeat(g.indent).bytes())
-}
-
 pub fn (mut g Gen) gen(f ast.File) {
 	g.file(f)
 }
