3807e5f5ebc77cdbd7114307bb42f33c351b88de
zakuro <z@kuro.red>
2021-03-19 16:49:06 +0900

    add errors test

---
+++ tests/errors_test.v
@@ -0,0 +1,16 @@
+import os
+
+fn test_compile_errors() ? {
+	dir := os.dir(@FILE)
+	filter := fn(s string) bool {
+		return s.contains('_err.') && ! s.ends_with('.out')
+	}
+	sources := (os.ls(dir) ?).filter(filter).map(os.join_path(dir, it))
+	for path in sources {
+		result := os.execute('v run cmd/vash $path')
+		out_path := path.trim_suffix(os.file_ext(path)) + '.out'
+		expected := os.read_file(out_path) ?
+		println('FILE: $path')
+		assert result.output == expected
+	}
+}
