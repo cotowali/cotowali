fe8a4ef4080a87c0c1d64b69a0f7744d9989e1ca
zakuro <z@kuro.red>
2021-04-01 16:49:47 +0900

    symbols: tweak test

--- cotowari/symbols/scope_test.v
+++ cotowari/symbols/scope_test.v
@@ -4,7 +4,7 @@ fn test_global_scope() {
 	mut s := new_global_scope()
 	assert s.is_global()
 	if t := s.lookup(unknown_type.name) {
-		assert t.id == Symbol(unknown_type).id
+		assert t.id == unknown_type.id
 	} else {
 		assert false
 	}
@@ -21,11 +21,21 @@ fn test_scope() ? {
 	assert s.children.len == 1
 	assert s.children[0].id == child.id
 
-	v1 := Symbol(new_var('v1'))
+	v1 := new_var('v1')
+	t1 := new_type('t1', PlaceholderTypeInfo{})
 
 	if registered := s.register(v1) {
 		assert v1.id == registered.id
 		assert (registered.scope() ?).id == s.id
+		assert registered is Var
+	} else {
+		assert false
+	}
+
+	if registered := s.register(t1) {
+		assert t1.id == registered.id
+		assert (registered.scope() ?).id == s.id
+		assert registered is Type
 	} else {
 		assert false
 	}
@@ -43,24 +53,24 @@ fn test_scope() ? {
 	}
 }
 
-fn test_nested_scope() ? {
+fn test_lookup() ? {
 	mut parent := new_global_scope()
 	mut child := parent.create_child('child')
 
-	name := 'v'
-	parent_v := Symbol(new_var(name))
-	child_v := Symbol(new_var(name))
+	name_v := 'v'
+	parent_v := new_var(name_v)
+	child_v := new_var(name_v)
 	assert parent_v.name == child_v.name
 	assert parent_v.id != child_v.id
 
 	parent.register(parent_v) ?
-	if found := parent.lookup(name) {
+	if found := parent.lookup(name_v) {
 		assert found.id == parent_v.id
 		assert found.id != child_v.id
 	} else {
 		assert false
 	}
-	if found := child.lookup(name) {
+	if found := child.lookup(name_v) {
 		assert found.id == parent_v.id
 		assert found.id != child_v.id
 	} else {
@@ -68,13 +78,13 @@ fn test_nested_scope() ? {
 	}
 
 	child.register(child_v) ?
-	if found := parent.lookup(name) {
+	if found := parent.lookup(name_v) {
 		assert found.id == parent_v.id
 		assert found.id != child_v.id
 	} else {
 		assert false
 	}
-	if found := child.lookup(name) {
+	if found := child.lookup(name_v) {
 		assert found.id != parent_v.id
 		assert found.id == child_v.id
 	} else {
