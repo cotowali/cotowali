e77b10d96c9fea5087ffc5f4a7490919d35b77a4
zakuro <z@kuro.red>
2021-04-01 23:34:01 +0900

    lexer: mod

--- cotowari/lexer/lexer.v
+++ cotowari/lexer/lexer.v
@@ -64,6 +64,7 @@ pub fn (mut lex Lexer) read() Token {
 		`-` { lex.new_token_with_consume(.op_minus) }
 		`*` { lex.new_token_with_consume(.op_mul) }
 		`/` { lex.new_token_with_consume(.op_div) }
+		`%` { lex.new_token_with_consume(.op_mod) }
 		`,` { lex.new_token_with_consume(.comma) }
 		`.` { lex.new_token_with_consume(.dot) }
 		`@` { lex.read_at_ident() }
