89d548afcce31d79be792e4d9d36a01140e8e960
zakuro <z@kuro.red>
2021-04-11 08:01:45 +0900

    tests: integration test runner

--- tests/run_test.v
+++ tests/run_test.v
@@ -1,19 +1,25 @@
 import os
 
-fn test_compile_errors() ? {
+fn test_run() ? {
 	dir := os.dir(@FILE)
 	filter := fn (s string) bool {
-		return s.contains('_err.') && !s.ends_with('.out')
+		return s.ends_with('.ri')
 	}
 	sources := (os.ls(dir) ?).filter(filter).map(os.join_path(dir, it))
 	assert os.execute('v cmd/ri').exit_code == 0
 	for path in sources {
 		println('$path')
-		result := os.execute('./cmd/ri/ri $path')
 		out_path := path.trim_suffix(os.file_ext(path)) + '.out'
 		expected := os.read_file(out_path) ?
 		println('FILE: $path')
-		assert result.exit_code != 0
-		assert result.output == expected
+		if path.ends_with('_err.ri') {
+			result := os.execute('./cmd/ri/ri $path')
+			assert result.exit_code != 0
+			assert result.output == expected
+		} else {
+			result := os.execute('./cmd/ri/ri run $path')
+			assert result.exit_code == 0
+			assert result.output == expected
+		}
 	}
 }
