40485d5a91de4564277caf2dd0fa97869816750a
zakuro <z@kuro.red>
2021-04-14 04:19:52 +0900

    errors: add SimplePrinter

--- cotowari/errors/printer.v
+++ cotowari/errors/printer.v
@@ -1,7 +1,18 @@
 module errors
 
 import io
+import os
 
 pub interface Printer {
 	print(io.Writer, Err)
+}
+
+pub struct SimplePrinter {}
+
+pub fn (p SimplePrinter) print(w io.Writer, err Err) {
+	s := err.source
+	file := os.file_name(s.path)
+	pos := err.pos
+	msg := '$file $pos.line,$pos.col: $err.msg\n'
+	w.write(msg.bytes()) or { panic(err) }
 }
