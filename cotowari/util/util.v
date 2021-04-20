acf65a594c4daf84348051d6d7b422f0558f6115
zakuro <z@kuro.red>
2021-04-20 17:28:42 +0900

    util: add escpape

--- cotowari/util/util.v
+++ cotowari/util/util.v
@@ -7,3 +7,13 @@ pub fn @in<T>(v T, low T, high T) bool {
 pub fn in2<T>(v T, low1 T, high1 T, low2 T, high2 T) bool {
 	return (low1 <= v && v <= high1) || (low2 <= v && v <= high2)
 }
+
+pub fn escape(s string, targets []string) string {
+	s2 := s.replace('\\', '\\\\')
+	mut reps := []string{cap: targets.len * 2}
+	for v in targets {
+		reps << v
+		reps << '\\' + v
+	}
+	return s2.replace_each(reps)
+}
