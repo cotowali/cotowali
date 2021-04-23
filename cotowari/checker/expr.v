78b47f819a40e1a5ec9b246a1f812bd3455280c7
zakuro <z@kuro.red>
2021-04-23 18:38:02 +0900

    checker: check infix_expr type mismatch

--- cotowari/checker/expr.v
+++ cotowari/checker/expr.v
@@ -2,12 +2,21 @@ module checker
 
 import cotowari.ast
 
-fn (c Checker) expr(expr ast.Expr) {
+fn (mut c Checker) expr(expr ast.Expr) {
 	match expr {
 		ast.CallFn {}
-		ast.InfixExpr {}
+		ast.InfixExpr { c.infix_expr(expr) }
 		ast.Literal {}
 		ast.Pipeline {}
 		ast.Var {}
+	}
+}
+
+fn (mut c Checker) infix_expr(expr ast.InfixExpr) {
+	left_type := expr.left.typ()
+	right_type := expr.right.typ()
+	if left_type.id != right_type.id {
+		c.error('mismatch type: `$left_type.name` (left), `$right_type.name` (right)',
+			ast.Expr(expr).pos())
 	}
 }
