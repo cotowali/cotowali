10fec132a386fd5d42e391832c9ef4dc18d689a5
zakuro <z@kuro.red>
2021-04-23 23:44:17 +0900

    tools: print ast requires single file

--- cmd/tools/ast.v
+++ cmd/tools/ast.v
@@ -1,7 +1,6 @@
 module tools
 
 import cli { Command }
-import strings
 import cotowari.parser
 
 const (
@@ -9,28 +8,16 @@ const (
 		name: 'ast'
 		description: 'print ast'
 		execute: fn (cmd Command) ? {
-			if cmd.args.len == 0 {
+			if cmd.args.len != 1 {
 				cmd.execute_help()
 				return
 			}
-			print_files_ast(cmd.args)
+			if f := parser.parse_file(cmd.args[0]) {
+				println(f)
+			} else {
+				println('ERROR')
+			}
 			return
 		}
 	}
 )
-
-fn print_files_ast(paths []string) {
-	mut sb := strings.new_builder(100)
-	sb.writeln('[')
-	for path in paths {
-		f := parser.parse_file(path) or {
-			sb.writeln('    ERROR')
-			continue
-		}
-		for line in f.str().split_into_lines() {
-			sb.writeln('    $line')
-		}
-	}
-	sb.writeln(']')
-	println(sb)
-}
