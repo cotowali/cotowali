00c3471fd0a86aa49b5274463395093827008370
zakuro <z@kuro.red>
2021-04-23 17:47:18 +0900

    integration_test

--- integration_test.v
+++ integration_test.v
@@ -1,11 +1,31 @@
 import os
 
+const skip_list = [
+	'examples/hello.ri'
+]
+
+fn is_target_file(s string) bool {
+	for skip in skip_list {
+		if s.ends_with(skip) {
+			return false
+		}
+	}
+	return s.ends_with('.ri')
+}
+
+fn get_sources(dirs ...string) []string {
+	mut res := []string{}
+	for dir in dirs {
+		res << (os.ls(dir) or { panic(err) }).map(os.join_path(dir, it)).filter(is_target_file)
+	}
+	return res
+}
+
 fn test_run() ? {
 	dir := os.dir(@FILE)
-	filter := fn (s string) bool {
-		return s.ends_with('.ri')
-	}
-	sources := (os.ls(dir) ?).filter(filter).map(os.join_path(dir, it))
+	examples_dir := os.join_path(dir, 'examples')
+	tests_dir := os.join_path(dir, 'tests')
+	sources := get_sources(examples_dir, tests_dir)
 	assert os.execute('v cmd/ric').exit_code == 0
 	for path in sources {
 		println('$path')
