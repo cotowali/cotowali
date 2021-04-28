468a283c78c6d2d5bd11fe43c8b88c22c473196e
zakuro <z@kuro.red>
2021-04-29 08:55:21 +0900

    symbols: merge bulitin.v into symbols.v

--- cotowari/symbols/symbols.v
+++ cotowari/symbols/symbols.v
@@ -109,3 +109,71 @@ pub fn (t TypeSymbol) kind() TypeKind {
 pub fn (v TypeSymbol) str() string {
 	return 'TypeSymbol{ typ: $v.typ, name: $v.name, kind: $v.kind() }'
 }
+
+// --- builtin --- //
+
+pub enum BuiltinTypeKey {
+	placeholder = 0
+	unknown
+	int
+	string
+	bool
+}
+
+pub fn builtin_type(key BuiltinTypeKey) Type {
+	return Type(int(key))
+}
+
+/*
+pub fn builtin_type(key BuiltinTypeKey) Type {
+	v := symbols.builtin_types[key] or { panic(unreachable) }
+	return v
+}
+*/
+
+pub const (
+	builtin_types = (fn () map[BuiltinTypeKey]Type {
+		k := fn (k BuiltinTypeKey) BuiltinTypeKey {
+			return k
+		}
+		t := fn (k BuiltinTypeKey) Type {
+			return builtin_type(k)
+		}
+		return map{
+			k(.placeholder): t(.placeholder)
+			k(.unknown):     t(.unknown)
+			k(.int):         t(.int)
+			k(.string):      t(.string)
+			k(.bool):        t(.bool)
+		}
+	}())
+		/*
+		builtin_types = (fn () map[BuiltinTypeKey]Type {
+		k := fn (k BuiltinTypeKey) BuiltinTypeKey {
+			return k
+		}
+		return map{
+			k(.unknown): Type{
+				id: 1
+				name: 'unknown'
+				info: UnknownTypeInfo{}
+			}
+			k(.int):     Type{
+				id: 2
+				name: 'int'
+				info: PrimitiveTypeInfo{}
+			}
+			k(.string):  Type{
+				id: 3
+				name: 'string'
+				info: PrimitiveTypeInfo{}
+			}
+			k(.bool):    Type{
+				id: 4
+				name: 'bool'
+				info: PrimitiveTypeInfo{}
+			}
+		}
+	}())
+		*/
+)
