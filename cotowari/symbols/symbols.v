ccd1aad37cd0c320b856a8a7962bab9fa31816e3
zakuro <z@kuro.red>
2021-04-29 08:49:45 +0900

    symbols: merge symbols, types and var

--- cotowari/symbols/symbols.v
+++ cotowari/symbols/symbols.v
@@ -24,3 +24,88 @@ pub fn (sym Symbol) full_name() string {
 		return name
 	}
 }
+
+// --- Var --- //
+
+pub struct Var {
+mut:
+	scope &Scope = 0
+pub:
+	name string
+	id   u64
+pub mut:
+	typ Type = builtin_type(.placeholder)
+}
+
+pub fn (v Var) str() string {
+	return 'Var{ name: $v.name, scope: ${Symbol(v).scope_str()}, typ: $v.typ }'
+}
+
+pub fn (v Var) full_name() string {
+	return Symbol(v).full_name()
+}
+
+pub fn (v Var) scope() ?&Scope {
+	return Symbol(v).scope()
+}
+
+// --- Type --- //
+
+pub type Type = int
+
+pub struct UnknownTypeInfo {}
+
+pub struct PrimitiveTypeInfo {}
+
+pub struct PlaceholderTypeInfo {
+	is_fn bool
+}
+
+pub struct FuncTypeInfo {
+	args []Type
+	ret  Type
+}
+
+pub type TypeInfo = PlaceholderTypeInfo | PrimitiveTypeInfo | UnknownTypeInfo
+
+pub struct TypeSymbol {
+pub:
+	typ  Type
+	name string
+	info TypeInfo //= TypeInfo(PlaceholderTypeInfo{})
+}
+
+pub fn (t TypeSymbol) is_fn() bool {
+	info := t.info
+	return match info {
+		PlaceholderTypeInfo { info.is_fn }
+		// FuncTypeInfo { true }
+		else { false }
+	}
+}
+
+pub enum TypeKind {
+	placeholder
+	unknown
+	primitive
+	func
+}
+
+// type kind
+[inline]
+fn tk(k TypeKind) TypeKind {
+	return k
+}
+
+pub fn (t TypeSymbol) kind() TypeKind {
+	return match t.info {
+		UnknownTypeInfo { tk(.unknown) }
+		PlaceholderTypeInfo { tk(.placeholder) }
+		PrimitiveTypeInfo { tk(.primitive) }
+		// FuncTypeInfo { tk(.func) }
+	}
+}
+
+pub fn (v TypeSymbol) str() string {
+	return 'TypeSymbol{ typ: $v.typ, name: \'$v.name\', kind: $v.kind().str() }'
+}
