64a1e9285871f02effefb7cbf9b07c8bfac35071
zakuro <z@kuro.red>
2021-04-29 09:21:44 +0900

    symbols: add struct Builtin

--- cotowari/symbols/symbols.v
+++ cotowari/symbols/symbols.v
@@ -120,60 +120,30 @@ pub enum BuiltinTypeKey {
 	bool
 }
 
+pub struct Builtin {
+pub:
+	types map[BuiltinTypeKey]Type
+}
+
 pub fn builtin_type(key BuiltinTypeKey) Type {
 	return Type(int(key))
 }
 
-/*
-pub fn builtin_type(key BuiltinTypeKey) Type {
-	v := symbols.builtin_types[key] or { panic(unreachable) }
-	return v
-}
-*/
-
 pub const (
-	builtin_types = (fn () map[BuiltinTypeKey]Type {
+	builtin = (fn () Builtin {
 		k := fn (k BuiltinTypeKey) BuiltinTypeKey {
 			return k
 		}
 		t := fn (k BuiltinTypeKey) Type {
 			return builtin_type(k)
 		}
-		return map{
+		types := map{
 			k(.placeholder): t(.placeholder)
 			k(.unknown):     t(.unknown)
 			k(.int):         t(.int)
 			k(.string):      t(.string)
 			k(.bool):        t(.bool)
 		}
+		return Builtin{types}
 	}())
-		/*
-		builtin_types = (fn () map[BuiltinTypeKey]Type {
-		k := fn (k BuiltinTypeKey) BuiltinTypeKey {
-			return k
-		}
-		return map{
-			k(.unknown): Type{
-				id: 1
-				name: 'unknown'
-				info: UnknownTypeInfo{}
-			}
-			k(.int):     Type{
-				id: 2
-				name: 'int'
-				info: PrimitiveTypeInfo{}
-			}
-			k(.string):  Type{
-				id: 3
-				name: 'string'
-				info: PrimitiveTypeInfo{}
-			}
-			k(.bool):    Type{
-				id: 4
-				name: 'bool'
-				info: PrimitiveTypeInfo{}
-			}
-		}
-	}())
-		*/
 )
