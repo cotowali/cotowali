6ebac6cae48ba233c47d7d2bcc60da3ff04e043f
zakuro <z@kuro.red>
2021-05-05 00:31:29 +0900

    symbols: add FunctionTypeInfo.signature

--- cotowari/symbols/type.v
+++ cotowari/symbols/type.v
@@ -82,14 +82,17 @@ pub fn (v TypeSymbol) str() string {
 	return 'TypeSymbol{ typ: $v.typ, name: $v.name, kind: $v.kind().str() }'
 }
 
+fn (f FunctionTypeInfo) signature(s &Scope) string {
+	args_str := f.args.map(s.must_lookup_type(it).name).join(', ')
+	return 'fn ($args_str) ${s.must_lookup_type(f.ret).name}'
+}
+
 pub fn (t TypeSymbol) fn_signature() ?string {
-	if t.info is FunctionTypeInfo {
-		f := t.info
-		s := t.scope() or { panic(unreachable) }
-		args_str := f.args.map(s.must_lookup_type(it).name).join(', ')
-		return 'fn ($args_str) ${s.must_lookup_type(f.ret).name}'
+	return if t.info is FunctionTypeInfo {
+		t.info.signature(t.scope() or { panic(unreachable) })
+	} else {
+		none
 	}
-	return none
 }
 
 // -- register / lookup --
