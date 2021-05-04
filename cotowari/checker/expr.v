dbc34141ec91638dc18826aa677d7c11fa85d977
zakuro <z@kuro.red>
2021-05-05 08:18:15 +0900

    checker: check unedfined function call

--- cotowari/checker/expr.v
+++ cotowari/checker/expr.v
@@ -3,8 +3,8 @@ module checker
 import cotowari.ast { Expr }
 
 fn (mut c Checker) expr(expr Expr) {
-	match expr {
-		ast.CallFn {}
+	match mut expr {
+		ast.CallFn { c.call_expr(mut expr) }
 		ast.InfixExpr { c.infix_expr(expr) }
 		ast.Literal {}
 		ast.Pipeline {}
@@ -18,5 +18,13 @@ fn (mut c Checker) infix_expr(expr ast.InfixExpr) {
 	right_ts := expr.right.type_symbol()
 	if left_ts.typ != right_ts.typ {
 		c.error('mismatch type: `$left_ts.name` (left), `$right_ts.name` (right)', Expr(expr).pos())
+	}
+}
+
+fn (mut c Checker) call_expr(mut expr ast.CallFn) {
+	name := expr.func.name()
+	func := expr.scope.lookup_var(name) or {
+		c.error('function `$name` is not defined', Expr(expr).pos())
+		return
 	}
 }
