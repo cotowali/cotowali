efb633b8aeb87310e6470bf999a3e2a726491c7f
zakuro <z@kuro.red>
2021-05-05 09:36:03 +0900

    improve integaration test runner

--- integration_test.v
+++ integration_test.v
@@ -21,37 +21,80 @@ fn get_sources(dirs ...string) []string {
 	return res
 }
 
+struct TestResult {
+mut:
+	file        string [required]
+	is_err_test bool   [required]
+	ok          bool   [required]
+	exit_code   int    [required]
+	output      string [required]
+	expected    string [required]
+}
+
+fn (result TestResult) print_fail_info() {
+	indent := ' '.repeat(2)
+	format_output := fn (text string) string {
+		indent := ' '.repeat(4)
+		return if text.len == 0 {
+			'${indent}__EMPTY__'
+		} else {
+			text.split_into_lines().map('$indent$it').join('\n')
+		}
+	}
+	println('[FAIL] $result.file')
+	println('${indent}exit_code: $result.exit_code')
+	println('${indent}output:')
+	println(format_output(result.output))
+	println('${indent}expected:')
+	println(format_output(result.expected))
+	println('')
+}
+
 fn test_run() ? {
 	dir := os.dir(@FILE)
 	examples_dir := os.join_path(dir, 'examples')
 	tests_dir := os.join_path(dir, 'tests')
 	sources := get_sources(examples_dir, tests_dir)
 	assert os.execute('v cmd/ric').exit_code == 0
+	bin := './cmd/ric/ric'
+	mut results := []TestResult{}
 	for path in sources {
-		println('$path')
 		out_path := path.trim_suffix(os.file_ext(path)) + '.out'
+		is_err_test := path.ends_with('_err.ri')
+
+		cmd_result := os.execute(if is_err_test { '$bin $path' } else { '$bin run $path' })
 		expected := os.read_file(out_path) or { '' }
-		println('FILE: $path')
-		if path.ends_with('_err.ri') {
-			result := os.execute('./cmd/ric/ric $path')
-			assert result.exit_code != 0
-			$if fix ? {
-				if result.output != expected {
-					os.write_file(out_path, result.output) ?
-				}
-			} $else {
-				assert result.output == expected
+		output := cmd_result.output
+		exit_code := cmd_result.exit_code
+		mut ok := true
+
+		$if fix ? {
+			if output != expected {
+				os.write_file(out_path, output) ?
 			}
-		} else {
-			result := os.execute('./cmd/ric/ric run $path')
-			assert result.exit_code == 0
-			$if fix ? {
-				if result.output != expected {
-					os.write_file(out_path, result.output) ?
-				}
-			} $else {
-				assert result.output == expected
-			}
+			ok = true
+		} $else {
+			ok = output == expected && (if is_err_test { exit_code != 0 } else { exit_code == 0 })
 		}
+		results << TestResult{
+			file: os.join_path(os.base(os.dir(path)), os.base(path))
+			is_err_test: is_err_test
+			ok: ok
+			exit_code: exit_code
+			output: output
+			expected: expected
+		}
+	}
+
+	mut ok := true
+	for result in results {
+		if result.ok {
+			continue
+		}
+		ok = false
+		result.print_fail_info()
+	}
+	if !ok {
+		assert false
 	}
 }
