35dcd0b6b9e435bdf566b881b1a6ff7491c88136
zakuro <z@kuro.red>
2021-05-05 12:51:18 +0900

    erros: add ErrorWithPos

--- cotowari/errors/error.v
+++ cotowari/errors/error.v
@@ -6,11 +6,19 @@ pub const (
 	unreachable = 'unreachable - This is a compiler bug.'
 )
 
+// Err represents cotowari compile error
 pub struct Err {
 pub:
 	source Source
 	pos    Pos
 	// Implements IError
+	msg  string
+	code int
+}
+
+pub struct ErrorWithPos {
+pub:
+	pos  Pos
 	msg  string
 	code int
 }
