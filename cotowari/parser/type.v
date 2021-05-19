a8c29f147cb4bd8a2b6b7cedf74fc2c87e099bd8
zakuro <z@kuro.red>
2021-05-19 18:04:58 +0900

    parser: parse array type

--- cotowari/parser/type.v
+++ cotowari/parser/type.v
@@ -2,8 +2,21 @@ module parser
 
 import cotowari.symbols { Type }
 
+fn (mut p Parser) parse_array_type() ?Type {
+	p.consume_with_assert(.l_bracket)
+	p.consume_with_check(.r_bracket) ?
+	elem := p.parse_type() ?
+	return p.scope.lookup_or_register_array_type(elem: elem).typ
+}
+
 fn (mut p Parser) parse_type() ?Type {
-	tok := p.consume_with_check(.ident) ?
-	typ := (p.scope.lookup_type(tok.text) or { return p.error(err, tok.pos) }).typ
-	return typ
+	match p.kind(0) {
+		.l_bracket {
+			return p.parse_array_type()
+		}
+		else {
+			tok := p.consume_with_check(.ident) ?
+			return (p.scope.lookup_type(tok.text) or { return p.error(err, tok.pos) }).typ
+		}
+	}
 }
