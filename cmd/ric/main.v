66bded952a99f0b9dfaeab3f9f3f47a5aec1fbed
zakuro <z@kuro.red>
2021-05-19 21:04:43 +0900

    ric: add --backend flag

--- cmd/ric/main.v
+++ cmd/ric/main.v
@@ -1,13 +1,20 @@
 module main
 
 import os
-import cli { Command }
+import cli { Command, Flag }
 import v.vmod
 import cotowari { compile, format_error }
-import cotowari.config { new_config }
+import cotowari.config { Config, backend_from_str, new_config }
 import cotowari.source { Source }
-import cotowari.errors
+import cotowari.errors { unreachable }
 import cmd.tools
+
+const backend_flag = Flag{
+	flag: .string
+	name: 'backend'
+	abbrev: 'b'
+	default_value: ['sh']
+}
 
 fn new_source_to_run(args []string) ?Source {
 	if args.len == 0 {
@@ -20,6 +27,16 @@ fn new_source_to_run(args []string) ?Source {
 	}
 }
 
+fn new_config_from_cmd(cmd Command) &Config {
+	mut config := new_config()
+	backend_str := cmd.flags.get_string(backend_flag.name) or { panic(unreachable) }
+	config.backend = backend_from_str(backend_str) or {
+		eprintln(err)
+		exit(1)
+	}
+	return config
+}
+
 fn execute_run(cmd Command) ? {
 	if cmd.args.len > 1 {
 		eprintln('too many source files')
@@ -29,7 +46,7 @@ fn execute_run(cmd Command) ? {
 		eprintln(err)
 		exit(1)
 	}
-	config := new_config()
+	config := new_config_from_cmd(cmd)
 	cotowari.run(s, config) or {
 		eprintln(err)
 		exit(1)
@@ -45,7 +62,7 @@ fn execute_compile(cmd Command) ? {
 		exit(1)
 	}
 	s := source.read_file(cmd.args[0]) ?
-	config := new_config()
+	config := new_config_from_cmd(cmd)
 	out := compile(s, config) or {
 		eprint(format_error(err, errors.PrettyFormatter{}))
 		exit(1)
@@ -60,6 +77,7 @@ fn main() {
 		description: mod.description
 		version: mod.version
 		execute: execute_compile
+		flags: [backend_flag]
 		commands: [
 			Command{
 				name: 'run'
