5d5e3074717f5888980e6256f4dfea45ba6f31e4
zakuro <z@kuro.red>
2021-05-20 21:45:35 +0900

    symbols: add lookup_array_type

--- cotowari/symbols/array.v
+++ cotowari/symbols/array.v
@@ -4,8 +4,15 @@ pub struct ArrayTypeInfo {
 	elem Type
 }
 
-pub fn (mut s Scope) lookup_or_register_array_type(info ArrayTypeInfo) TypeSymbol {
+fn (info ArrayTypeInfo) typename(s &Scope) string {
 	elem_ts := s.must_lookup_type(info.elem)
-	typename := '[]$elem_ts.name'
-	return s.lookup_or_register_type(name: typename, info: info)
+	return '[]$elem_ts.name'
+}
+
+pub fn (mut s Scope) lookup_or_register_array_type(info ArrayTypeInfo) TypeSymbol {
+	return s.lookup_or_register_type(name: info.typename(s), info: info)
+}
+
+pub fn (s Scope) lookup_array_type(info ArrayTypeInfo) ?TypeSymbol {
+	return s.lookup_type(info.typename(s))
 }
