024f61bf2f368e27bd8e58ad283d51f5a1368d29
zakuro <z@kuro.red>
2021-05-23 11:33:32 +0900

    emit: remove unused code

--- cotowari/emit/sh/array.v
+++ cotowari/emit/sh/array.v
@@ -8,24 +8,6 @@ fn (mut e Emitter) array_literal(expr ast.ArrayLiteral, opt ExprOpt) {
 	e.array(name, opt)
 }
 
-type ArrayAssignValue = ast.ArrayLiteral | string
-
-fn (mut e Emitter) array_assign(name string, value ArrayAssignValue) {
-	match value {
-		ast.ArrayLiteral {
-			e.code.write('array_assign "$name"')
-			for elem in value.elements {
-				e.code.write(' ')
-				e.expr(elem, as_command: false)
-			}
-			e.code.writeln('')
-		}
-		string {
-			e.code.writeln('array_assign "$name" \$(array_elements "$value")')
-		}
-	}
-}
-
 fn (mut e Emitter) array(name string, opt ExprOpt) {
 	if opt.as_command {
 		e.code.writeln('echo \$(eval echo \$(array_elements $name) )')
