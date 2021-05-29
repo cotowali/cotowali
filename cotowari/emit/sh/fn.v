12a61cbba1756085af999da3d527929299b708c8
zakuro <z@kuro.red>
2021-05-29 14:14:14 +0900

    tweak

--- cotowari/emit/sh/fn.v
+++ cotowari/emit/sh/fn.v
@@ -1,8 +1,8 @@
 module sh
 
-import cotowari.ast
+import cotowari.ast { CallFn, FnDecl }
 
-fn (mut e Emitter) call_fn(expr ast.CallFn, opt ExprOpt) {
+fn (mut e Emitter) call_fn(expr CallFn, opt ExprOpt) {
 	if !opt.as_command {
 		e.write('\$(')
 	}
@@ -18,7 +18,7 @@ fn (mut e Emitter) call_fn(expr ast.CallFn, opt ExprOpt) {
 	}
 }
 
-fn (mut e Emitter) fn_decl(node ast.FnDecl) {
+fn (mut e Emitter) fn_decl(node FnDecl) {
 	if !node.has_body {
 		e.writeln('')
 		params_str := node.params.map('$it.sym.name').join(', ')
@@ -36,7 +36,7 @@ fn (mut e Emitter) fn_decl(node ast.FnDecl) {
 		e.cur_fn = old_cur_fn
 	}
 
-	e.write_block({ open: '${node.name}() {', close: '}' }, fn (mut e Emitter, node ast.FnDecl) {
+	e.write_block({ open: '${node.name}() {', close: '}' }, fn (mut e Emitter, node FnDecl) {
 		for i, param in node.params {
 			e.assign(param.out_name(), '\$${i + 1}', param.type_symbol())
 		}
