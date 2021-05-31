39120165d7859e15ed39365c92f2dc2bdf1a8f7b
zakuro <z@kuro.red>
2021-05-31 19:46:00 +0900

    errors: add ErrWithPos

--- cotowari/errors/error.v
+++ cotowari/errors/error.v
@@ -1,6 +1,7 @@
 module errors
 
 import cotowari.source { Pos, Source }
+import cotowari.token { Token }
 
 pub const (
 	unreachable = 'unreachable - This is a compiler bug.'
@@ -14,6 +15,24 @@ pub:
 	// Implements IError
 	msg  string
 	code int
+}
+
+pub struct ErrWithToken {
+pub:
+	source Source
+	token  Token
+	// Implements IError
+	msg  string
+	code int
+}
+
+pub fn (err ErrWithToken) to_err() Err {
+	return {
+		source: err.source
+		pos: err.token.pos
+		msg: err.msg
+		code: err.code
+	}
 }
 
 pub struct ErrorWithPos {
