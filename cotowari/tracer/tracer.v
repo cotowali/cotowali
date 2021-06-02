7d5e80267cdc23d9e9767e466de5761297cb135d
zakuro <z@kuro.red>
2021-06-02 17:10:01 +0900

    tracer: object

--- cotowari/tracer/tracer.v
+++ cotowari/tracer/tracer.v
@@ -42,3 +42,29 @@ pub fn (mut t Tracer) begin_fn(name string, args ...string) {
 pub fn (mut t Tracer) end_fn() {
 	t.indent--
 }
+
+pub fn (mut t Tracer) write_fields(fields map[string]string) {
+	for k, v in fields {
+		t.write_field(k, v)
+	}
+}
+
+pub fn (mut t Tracer) write_field(name string, v string) {
+	t.writeln('$name: $v')
+}
+
+pub fn (mut t Tracer) write_object(name string, fields map[string]string) {
+	t.begin_object(name)
+	t.write_fields(fields)
+	t.end_object()
+}
+
+pub fn (mut t Tracer) begin_object(name string) {
+	t.writeln('$name {')
+	t.indent++
+}
+
+pub fn (mut t Tracer) end_object() {
+	t.indent--
+	t.writeln('}')
+}
