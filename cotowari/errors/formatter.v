3e44788b091cfd9933475b1dd23cbc34b63ae747
zakuro <z@kuro.red>
2021-06-03 17:19:03 +0900

    errors: use Source.file_name

--- cotowari/errors/formatter.v
+++ cotowari/errors/formatter.v
@@ -1,7 +1,5 @@
 module errors
 
-import os
-
 pub interface Formatter {
 	format(Err) string
 }
@@ -10,21 +8,19 @@ pub struct SimpleFormatter {}
 
 pub fn (p SimpleFormatter) format(err Err) string {
 	s := err.source
-	file := os.file_name(s.path)
 	pos := err.pos
-	return '$file $pos.line,$pos.col: $err.msg\n'
+	return '$s.file_name() $pos.line,$pos.col: $err.msg\n'
 }
 
 pub struct PrettyFormatter {}
 
 pub fn (p PrettyFormatter) format(err Err) string {
 	s := err.source
-	file := os.file_name(s.path)
 	pos := err.pos
 	// TODO: More pretty
 	code := s.slice(pos.i, pos.i + pos.len).clone()
 	underline_len := utf8_str_visible_length(code)
-	l1 := '$file $pos.line,$pos.col: $err.msg'
+	l1 := '$s.file_name() $pos.line,$pos.col: $err.msg'
 	l2 := '  > ' + code
 	l3 := '    ' + '^'.repeat(underline_len)
 	return '$l1\n$l2\n$l3\n'
