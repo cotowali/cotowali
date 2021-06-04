a359fcc3915c065b236e48aacb1ab2f5c98cb83d
zakuro <z@kuro.red>
2021-06-04 21:11:20 +0900

    util: add const_max / const_min

--- cotowari/util/math.v
+++ cotowari/util/math.v
@@ -1,5 +1,33 @@
 module util
 
+import math
+
+pub fn const_max<T>() T {
+	$if T is u32 {
+		return u32(math.max_u32)
+	} $else $if T is u64 {
+		return u64(math.max_u64)
+	} $else $if T is int {
+		return int(math.max_i32)
+	} $else $if T is i64 {
+		return i64(math.max_i64)
+	}
+	panic('Invalid type')
+}
+
+pub fn const_min<T>() T {
+	$if T is u32 {
+		return u32(0)
+	} $else $if T is u64 {
+		return u64(0)
+	} $else $if T is int {
+		return int(math.min_i32)
+	} $else $if T is i64 {
+		return i64(math.min_i64)
+	}
+	panic('Invalid type')
+}
+
 pub fn abs<T>(v T) T {
 	if v < 0 {
 		return -v
