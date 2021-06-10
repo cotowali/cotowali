80fccda96b0fa99b139e0a942a9fd794e6d91dc1
zakuro <z@kuro.red>
2021-06-11 08:53:49 +0900

    errors: add Errors

--- cotowari/errors/errors.v
+++ cotowari/errors/errors.v
@@ -18,6 +18,11 @@ pub:
 	code int
 }
 
+pub fn (lhs Err) < (rhs Err) bool {
+	lhs_path, rhs_path := lhs.source.path, rhs.source.path
+	return lhs_path < rhs_path || (lhs_path == rhs_path && lhs.pos.i < rhs.pos.i)
+}
+
 pub struct ErrWithToken {
 pub:
 	source &Source
@@ -41,4 +46,47 @@ pub:
 	pos  Pos
 	msg  string
 	code int
+}
+
+pub struct Errors {
+mut:
+	i    int // for iter
+	list []Err
+
+	has_syntax_error bool
+}
+
+pub fn (mut e Errors) push(err Err) Err {
+	if err.is_syntax_error {
+		e.has_syntax_error = true
+	}
+	e.list << err
+	return err
+}
+
+[inline]
+pub fn (e Errors) has_syntax_error() bool {
+	return e.has_syntax_error
+}
+
+pub fn (mut e Errors) sort() {
+	e.list.sort()
+}
+
+pub fn (mut e Errors) list() []Err {
+	e.sort()
+	return e.list
+}
+
+pub fn (mut e Errors) next() ?Err {
+	if e.i == 0 {
+		e.sort()
+	}
+	if e.i >= e.list.len {
+		e.i = 0
+		return none
+	}
+	err := e.list[e.i]
+	e.i++
+	return err
 }
