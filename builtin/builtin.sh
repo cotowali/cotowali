a4b6b4e69b306237ba2df9d4bb2beb96816159fb
zakuro <z@kuro.red>
2021-06-17 17:01:07 +0900

    remove truthy / falsy

--- builtin/builtin.sh
+++ builtin/builtin.sh
@@ -1,15 +1,3 @@
 # cotowari builtin
 
-truthy() {
-  ! falsy "$1"
-}
-
-falsy() {
-  [ -z "$1" ] \
-    || [ "$1" = "false" ] \
-    || [ "$1" = "False" ] \
-    || [ "$1" = "FALSE" ] \
-    || [ "$1" = "0" ]
-}
-
 # end cotowari builtin
