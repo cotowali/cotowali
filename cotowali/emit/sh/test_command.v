d7d31c064a38ebfb0c6beef7a4243df7b095e69d
zakuro <z@kuro.red>
2021-07-06 16:19:56 +0900

    fix invalid declaration

--- cotowali/emit/sh/test_command.v
+++ cotowali/emit/sh/test_command.v
@@ -10,6 +10,6 @@ fn (mut e Emitter) sh_test_cond_is_true(expr ExprOrString) {
 	e.sh_test_cond_infix(expr, ' = ', "'true'")
 }
 
-fn (mut e Emitter) sh_test_command<T>(f fn (mut e Emitter), v T) {
+fn (mut e Emitter) sh_test_command<T>(f fn (mut Emitter, T), v T) {
 	e.write_inline_block({ open: '[ ', close: ' ]' }, f, v)
 }
