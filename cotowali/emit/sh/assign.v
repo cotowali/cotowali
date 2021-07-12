3150056503d7b2fe423b0df8fcd607473edcb764
zakuro <z@kuro.red>
2021-07-12 14:53:03 +0900

    emit: map assign

--- cotowali/emit/sh/assign.v
+++ cotowali/emit/sh/assign.v
@@ -7,6 +7,8 @@ module sh
 
 import cotowali.ast
 import cotowali.symbols { TypeSymbol }
+import cotowali.errors { unreachable }
+import cotowali.util { panic_and_value }
 
 fn (mut e Emitter) array_assign(name string, value ExprOrString) {
 	match value {
@@ -33,19 +35,40 @@ fn (mut e Emitter) array_assign(name string, value ExprOrString) {
 	}
 }
 
-fn (mut e Emitter) assign(name string, value ExprOrString, ts TypeSymbol) {
-	if ts.kind() == .array {
-		e.array_assign(name, value)
-		return
-	}
+fn (mut e Emitter) map_assign(name string, value ExprOrString) {
+	e.write('map_copy ')
+	e.map(name, {})
+	e.write(' ')
 	match value {
 		string {
-			e.writeln('$name="$value"')
+			e.map(value, {})
+			e.writeln('')
 		}
 		ast.Expr {
-			e.write('$name=')
-			e.expr(value, {})
-			e.writeln('')
+			e.expr(value, writeln: true)
+		}
+	}
+}
+
+fn (mut e Emitter) assign(name string, value ExprOrString, ts TypeSymbol) {
+	match ts.kind() {
+		.array {
+			e.array_assign(name, value)
+		}
+		.map {
+			e.map_assign(name, value)
+		}
+		else {
+			match value {
+				string {
+					e.writeln('$name="$value"')
+				}
+				ast.Expr {
+					e.write('$name=')
+					e.expr(value, {})
+					e.writeln('')
+				}
+			}
 		}
 	}
 }
@@ -53,7 +76,11 @@ fn (mut e Emitter) assign(name string, value ExprOrString, ts TypeSymbol) {
 fn (mut e Emitter) assign_stmt(node ast.AssignStmt) {
 	if node.left is ast.IndexExpr {
 		name := e.ident_for(node.left.left)
-		e.write('array_set $name ')
+		e.write(match node.left.left.type_symbol().kind() {
+			.array { 'array_set $name ' }
+			.map { 'map_set $name ' }
+			else { panic_and_value(unreachable('invalid index left'), '') }
+		})
 		e.expr(node.left.index, {})
 		e.write(' ')
 		e.expr(node.right, writeln: true)
