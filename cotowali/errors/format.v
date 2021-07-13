fe5416bdd78488c544048fb3da96e776f2f1694c
zakuro <z@kuro.red>
2021-07-14 00:46:39 +0900

    PrettyFormatter prints line number

--- cotowali/errors/format.v
+++ cotowali/errors/format.v
@@ -26,8 +26,9 @@ pub fn (p PrettyFormatter) format(err Err) string {
 	pos := err.pos
 	line := s.line(pos.line)
 	l1 := '$s.file_name() $pos.line,$pos.col: $err.msg'
-	l2 := '  > ' + line
-	l3 := '    ' + ' '.repeat(pos.col - 1) + '^'.repeat(pos.last_col - pos.col + 1)
+	l2 := '${pos.line:5d}| ' + line
+	l3 := '     | ' + ' '.repeat(pos.col - 1) + '^'.repeat(pos.last_col - pos.col + 1)
+	underline_len := utf8_str_visible_length(s.slice(pos.i, pos.i + pos.len))
 	return '$l1\n$l2\n$l3\n'
 }
 
