16b8a5f99da84f1f450e582948fa468e466a8aef
zakuro <z@kuro.red>
2021-08-23 13:34:02 +0900

    kuqqi: init did_open

--- kuqi/kuqi.v
+++ kuqi/kuqi.v
@@ -82,6 +82,7 @@ fn (mut q Kuqi) dispatch(payload string) ? {
 	match request.method {
 		'initialized' { q.log_message('initialized Kuqi', .log) }
 		'shutdown' { q.shutdown(id) }
+		lsp.method_did_open { q.did_open(id, q.decode<lsp.DidOpenTextDocumentParams>(params) ?) }
 		else {}
 	}
 }
---
+++ kuqi/text_document.v
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 zakuro <z@kuro.red>. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+module kuqi
+
+import lsp
+
+fn (mut q Kuqi) did_open(id int, params lsp.DidOpenTextDocumentParams) {
+	q.log_message('did open: { id: $id, uri: $params.text_document.uri }', .log)
+}
