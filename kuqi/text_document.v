09fd846f7f67440ded42524c8c21401c7fd94dc8
zakuro <z@kuro.red>
2021-08-24 11:33:42 +0900

    kuqi: register source to context

--- kuqi/text_document.v
+++ kuqi/text_document.v
@@ -6,18 +6,31 @@
 module kuqi
 
 import lsp
+import cotowali.source { new_source }
 
 fn (mut q Kuqi) did_open(id int, params lsp.DidOpenTextDocumentParams) {
 	q.log_message('did open: { id: $id, uri: $params.text_document.uri }', .log)
 	q.show_diagnostics(params.text_document.uri)
+	document := params.text_document
+	path := document.uri.path()
+	if path !in q.ctx.sources {
+		q.ctx.sources[path] = new_source(path, document.text)
+	}
 }
 
 fn (mut q Kuqi) did_change(id int, params lsp.DidChangeTextDocumentParams) {
 	q.log_message('did change: { id: $id, uri: $params.text_document.uri }', .log)
+
+	path := params.text_document.uri.path()
+	text := params.content_changes[0].text
+	q.ctx.sources[path] = new_source(path, text)
 }
 
 fn (mut q Kuqi) did_close(id int, params lsp.DidCloseTextDocumentParams) {
 	q.log_message('did close: { id: $id, uri: $params.text_document.uri }', .log)
+
+	path := params.text_document.uri.path()
+	q.ctx.sources.delete(path)
 }
 
 fn (mut q Kuqi) did_save(id int, params lsp.DidSaveTextDocumentParams) {
