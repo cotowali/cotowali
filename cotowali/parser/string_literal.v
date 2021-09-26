ea17712feab0ffd3e5350aaed632702ccadf199a
zakuro <z@kuro.red>
2021-09-26 09:21:00 +0900

    implement `"\\"`

--- cotowali/parser/string_literal.v
+++ cotowali/parser/string_literal.v
@@ -108,6 +108,7 @@ fn (mut p Parser) parse_double_quote_string_literal() ?ast.StringLiteral {
 		}
 		match p.kind(0) {
 			.string_literal_content_text, .string_literal_content_escaped_dollar,
+			.string_literal_content_escaped_back_slash,
 			.string_literal_content_escaped_double_quote, .string_literal_content_escaped_newline {
 				contents << p.consume()
 			}
