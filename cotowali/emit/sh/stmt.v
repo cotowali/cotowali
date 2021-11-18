2e44293cad2cac2c79edc3297f4e691607b97436
zakuro <z@kuro.red>
2021-11-18 09:10:40 +0900

    cleanup

--- cotowali/emit/sh/stmt.v
+++ cotowali/emit/sh/stmt.v
@@ -42,12 +42,14 @@ fn (mut e Emitter) stmt(stmt Stmt) {
 }
 
 fn (mut e Emitter) expr_stmt(stmt ast.Expr) {
-	discard_stdout := e.inside_fn && if stmt is ast.CallExpr {
-		e.cur_fn.function_info().ret != builtin_type(.void)
-	} else if stmt is ast.Pipeline {
-		!stmt.has_redirect()
+	discard_stdout := if cur_fn := e.cur_fn() {
+		match stmt {
+			ast.CallExpr { cur_fn.function_info().ret != builtin_type(.void) }
+			ast.Pipeline { !stmt.has_redirect() }
+			else { true }
+		}
 	} else {
-		true
+		false
 	}
 	e.expr(stmt, mode: .command, discard_stdout: discard_stdout, writeln: true)
 }
