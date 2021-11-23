cc93c10c85d17dd9e8767e41bc50803227ffb64e
zakuro <z@kuro.red>
2021-11-23 11:00:38 +0900

    add util.is_relative_path

---
+++ cotowali/util/path.v
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 zakuro <z@kuro.red>. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+module util
+
+import os
+
+pub fn is_relative_path(path string) bool {
+	return (path == '' || path == '.')
+		|| (path.starts_with('.$os.path_separator') || path.starts_with('..$os.path_separator'))
+}
