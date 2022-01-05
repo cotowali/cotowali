04bec93415d982dccf8a4af9b87f280d988599d0
zakuro <z@kuro.red>
2022-01-05 12:37:53 +0900

    himorogi: no_builtin for now

--- cmd/himorogi/main.v
+++ cmd/himorogi/main.v
@@ -19,6 +19,7 @@ const (
 fn execute(cmd Command) ? {
 	mut ctx := cmdutil.new_ctx_from_cmd(cmd)
 	ctx.config.backend = .himorogi
+	ctx.config.no_builtin = true
 	s, args := cmdutil.parse_args(cmd.args) or {
 		eprintln(err)
 		exit(1)
