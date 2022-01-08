884b5dd5841f881584763416b8ae3f043d0f47fd
zakuro <z@kuro.red>
2022-01-08 12:07:14 +0900

    himorogi: test `+` and `-`

--- tests/tester.v
+++ tests/tester.v
@@ -29,7 +29,7 @@ const (
 		'posix_test.li',
 		'glob_test.li',
 	]
-	himorogi_list        = ['assert_minimal.li', 'empty.li']
+	himorogi_list        = ['assert_minimal.li', 'empty.li', 'expr/int_test.li', 'expr/float_test.li']
 	use_test_runner_list = ['test_runner_test.li', 'std/assert.li']
 	slow_list            = os.glob('tests/require_remote/*') ?
 )
